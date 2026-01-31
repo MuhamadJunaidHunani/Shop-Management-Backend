2ff83f2076d240781eff84fcb328fe46928e3f77
ubaidullahshahid <146563446+ubaidullahshahid@users.noreply.github.com>
2026-01-31 20:02:12 +0500

    complete get product api

---
+++ Controllers/Product/GetProduct.js
@@ -0,0 +1,34 @@
+const ProductModel = require("../../Models/ProductModel");
+
+const getProduct = async (req, res) => {
+  try {
+    const { id } = req.query;
+
+    const data = id
+      ? await ProductModel.findById(id)
+      : await ProductModel.find().sort({ createdAt: -1 });
+
+    if (!data || (Array.isArray(data) && data.length === 0)) {
+      return res.status(404).json({
+        success: false,
+        message: id ? "Product not found" : "No products found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: id
+        ? "Product fetched successfully"
+        : "All products fetched successfully",
+      data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = getProduct;
--- Routes/Routes.js
+++ Routes/Routes.js
@@ -5,6 +5,7 @@ const tokenChecker = require("../MiddleWares/TokenChecker");
 const getMe = require("../Controllers/Auth/getMe");
 const addProduct = require("../Controllers/Product/AddProduct");
 const updateProduct = require("../Controllers/Product/UpdateProduct");
+const getProduct = require("../Controllers/Product/GetProduct");
 
 // post APIs
 routes.post("/login", loginUser);
@@ -15,5 +16,6 @@ routes.put("/update-product/:id", tokenChecker, updateProduct);
 
 // get APIs
 routes.get("/get-me", tokenChecker, getMe);
-
+routes.get("/get-product", tokenChecker, getProduct);
+ 
 module.exports = { routes };
